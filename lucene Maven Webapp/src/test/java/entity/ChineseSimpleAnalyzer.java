1c44004532cb57df1c462ea97f5ff9da5bab9028
Saka <Saka@Saka-PC.mshome.net>
2016-06-26 00:06:22 +0800

    commit

--- lucene Maven Webapp/src/test/java/entity/ChineseSimpleAnalyzer.java
+++ lucene Maven Webapp/src/test/java/entity/ChineseSimpleAnalyzer.java
@@ -3,20 +3,27 @@
  */
 package entity;
 
-import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.Reader;
+import java.io.StringReader;
 
-import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.core.LowerCaseTokenizer;
+import org.apache.lucene.analysis.core.StopFilter;
+import org.apache.lucene.analysis.util.StopwordAnalyzerBase;
+
+import com.spreada.utils.chinese.ZHConverter;
 
 /**
- * <p><b>Description:</b> </p>
+ * <p><b>Description:</b> 该自定义Analyzer主要用于中文繁体转换成简体，以及英文名转为小写</p>
  * <p><b>Date:</b> 2016年6月24日下午3:41:08</p>
  * <p><b>Copyright:</b> Copyright (c) 2016</p>
  * <p><b>Email:</b> jstxwh868@126.com</p>
  * @author Saka
  * @version 1.0
  */
-public class ChineseSimpleAnalyzer extends Analyzer {
+public class ChineseSimpleAnalyzer extends StopwordAnalyzerBase {
 
 	@Override
 	public int getPositionIncrementGap(String fieldName) {
@@ -27,16 +34,26 @@ public class ChineseSimpleAnalyzer extends Analyzer {
 	@Override
 	protected Reader initReader(String fieldName, Reader reader) {
 		// TODO Auto-generated method stub
-		while(reader.ready()){
-			
+		int acc=0;
+		StringBuilder sb=new StringBuilder();
+		try {
+			while((acc=reader.read())!=-1)
+				sb.append((char)acc);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new RuntimeException("initReader encountered an error!");
 		}
+		ZHConverter zh=ZHConverter.getInstance(ZHConverter.SIMPLIFIED);
+		reader=new StringReader(zh.convert(sb.toString()));
 		return super.initReader(fieldName, reader);
 	}
 
 	@Override
 	protected TokenStreamComponents createComponents(String fieldName) {
 		// TODO Auto-generated method stub
-		return null;
+		Tokenizer tokenizer=new LowerCaseTokenizer();
+		TokenStream tokenStream=new StopFilter(tokenizer,stopwords);
+		return new TokenStreamComponents(tokenizer,tokenStream);
 	}
-
 }
---
+++ lucene Maven Webapp/src/test/java/test/SimpleTest.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package test;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.junit.Test;
+
+import com.spreada.utils.chinese.ZHConverter;
+
+import entity.ChineseSimpleAnalyzer;
+
+/**
+ * <p><b>Description:</b> 中文简繁体互转</p>
+ * <p><b>Date:</b> 2016年6月24日上午11:07:04</p>
+ * <p><b>Copyright:</b> Copyright (c) 2016</p>
+ * <p><b>Email:</b> jstxwh868@126.com</p>
+ * @author Saka
+ * @version 1.0
+ */
+public class SimpleTest {
+	public static void main(String[] args) throws UnsupportedEncodingException {
+		ZHConverter converter = ZHConverter.getInstance(ZHConverter.SIMPLIFIED);
+		String sentence = converter.convert("中華人民共和國");
+		System.out.println(sentence);
+	}
+	/**
+	 * <p><b>Description:</b> 测试自定义中文繁体转简体分词器</p>
+	 * <p><b>Date:</b> 2016年6月25日下午12:02:50</p>
+	 * @throws IOException
+	 */
+	@Test
+	public void testReader() throws IOException{
+		Analyzer analyzer=new ChineseSimpleAnalyzer();
+		TokenStream stream = analyzer.tokenStream("test", "吉沢明步、吉澤明步,Yoshizawa Akiho");
+		stream.reset();
+		while(stream.incrementToken()){
+			CharTermAttribute attribute = stream.getAttribute(CharTermAttribute.class);
+			System.out.println(attribute.toString());
+		}
+		stream.end();
+		analyzer.close();
+	}
+}
